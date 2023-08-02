6cf5d04620531ba8eff41f65f3f7ab5c148e2db2
linhung0319 <linhung0319@gmail.com>
2023-08-02 16:34:30 +0800

    chore: set USER_AGENT='Googlebot'

--- novelCrawler/settings.py
+++ novelCrawler/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = "novelCrawler.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "novelCrawler (+http://www.yourdomain.com)"
+USER_AGENT = "Googlebot" 
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
